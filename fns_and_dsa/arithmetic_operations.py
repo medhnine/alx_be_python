a8b3b2e7fcc8d80bf7a3a390d3b699734b94d728
Mohamed HNINE <43042061+medhnine@users.noreply.github.com>
2025-06-08 02:08:34 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,6 +8,6 @@ def perform_operation(num1, num2, operation):
       return num1 * num2
     case _ if operation ==  'divide':
       if num2 > 0:
-        return num1 / num2
+        return num1 != num2
       else:
         return "Error: Division by zero"
